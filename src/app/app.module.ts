fdd56d74e3439ab78956657b10f3c34f6dab7b2c
Oscar Mireles Garcia <oscar.mireles@developmentservices.com.mx>
2024-04-23 09:47:39 -0600

    Agregando servicio httpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
@@ -11,8 +12,9 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
     SharedModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'shared-search-box',
@@ -8,4 +8,12 @@ export class SearchBoxComponent {
 
   @Input()
   public placeHolder: string = '';
+
+  @Output()
+  public onValue = new EventEmitter<string>();
+
+  emitValue(value: string): void {
+    this.onValue.emit(value);
+  }
+
 }
