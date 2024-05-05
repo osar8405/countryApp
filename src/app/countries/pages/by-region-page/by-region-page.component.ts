d1ee5d1168685ead43da9ba82c90792083f58657
Oscar Mireles Garcia <osar8405@gmail.com>
2024-05-04 21:07:29 -0600

    Agregando type, para las regiones, preparando cache

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CountriesService } from '../../services/coutries.service';
-import { Country } from '../../interfaces/country';
+import { Country } from '../../interfaces/country.interface';
+import { Region } from '../../interfaces/region.type';
 
 @Component({
   selector: 'app-by-region-page',
@@ -11,11 +12,14 @@ import { Country } from '../../interfaces/country';
 export class ByRegionPageComponent {
 
   public countries: Country[] = [];
+  public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  public selectedRegion?: Region;
   
   constructor(private countriesService: CountriesService) {
 
   }
-  searchByRegion(region: string): void {
+  searchByRegion(region: Region): void {
+    this.selectedRegion = region;
     this.countriesService.searchRegion(region)
     .subscribe(countries => {
       this.countries = countries;
--- src/app/countries/services/coutries.service.ts
+++ src/app/countries/services/coutries.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, catchError, delay, map, of } from 'rxjs';
-import { Country } from '../interfaces/country';
+import { Country } from '../interfaces/country.interface';
+import { CacheStore } from '../interfaces/cache-store.interface';
 
 @Injectable({ providedIn: 'root' })
 export class CountriesService {
 
     private apiURL: string = 'https://restcountries.com/v3.1';
+    public cahceStore: CacheStore = {
+        byCapital: {term: '', countries: []},
+        byCountries: {term: '', countries: []},
+        byRegion: {region: '', countries: []},
+    }
 
     constructor(private http: HttpClient) { }
 
